4ea48f0fb202df0f28cb4088d13d255b397ab2d8
Colum Kelly <columk1@users.noreply.github.com>
2023-12-21 22:06:19 -0800

    Remove unused markup from about page

--- src/routes/about/About.jsx
+++ src/routes/about/About.jsx
@@ -1,33 +1,16 @@
 import styles from './About.module.css'
-import CartItem from '../../components/CartItem/CartItem'
 import { useOutletContext, Link } from 'react-router-dom'
 
 const About = () => {
-  const cartItems = []
-  // const { cartItems, setCartItems } = useOutletContext()
-
   return (
-    <section className={styles.cart}>
+    <section>
       <h2 className={styles.title}>About me</h2>
-      {cartItems.length === 0 ? (
-        <div className={styles.status}>
-          <h3>Nothing to see here</h3>
-          <Link to={'/'}>
-            <button>Home</button>
-          </Link>
-        </div>
-      ) : (
-        <>
-          <div className={styles.cartItems}>
-            {cartItems.map((item) => (
-              <CartItem key={item.id} product={item} />
-            ))}
-          </div>
-          <div className={styles.checkout}>
-            <button className={styles.btn}>Home</button>
-          </div>
-        </>
-      )}
+      <div className={styles.status}>
+        <h3>Nothing to see here</h3>
+        <Link to={'/'}>
+          <button>Home</button>
+        </Link>
+      </div>
     </section>
   )
 }

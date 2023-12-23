a79e5c40ec53069d142b43f9f3c339fe22309d06
Colum Kelly <columk1@users.noreply.github.com>
2023-12-22 18:56:17 -0800

    Add Font Awesome packages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import styles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <h3>Footer</h3>
+    <footer className={styles.footer}>
+      <p>columkelly 2023</p>
+      <span className={styles.iconWrapper}>
+        <a href='https://github.com/columk1' target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+        </a>
+      </span>
     </footer>
   )
 }

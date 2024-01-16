6a6aabb40468d48c4686d1d0b3c348f223b1e63f
Colum Kelly <columk1@users.noreply.github.com>
2024-01-16 11:24:19 -0800

    Fix url syntax for images to work in deployment

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,33 +1,6 @@
 import styles from './Projects.module.css'
 import Project from '../Project/Project'
-
-const projects = [
-  {
-    title: 'Blog API',
-    description:
-      'A REST API for my personal blog using Node, Express and MongoDB. I built a CMS to interact with the API using React and Tailwind. It allows the admin to preview the markdown while editing. Authentication is handled using JWTs.',
-    image: 'src/assets/blog-cms.jpg',
-    tags: ['Node', 'Express', 'MongoDB', 'React', 'Tailwind'],
-    github: 'https://github.com/columk1/blog-api',
-    url: 'https://cms.columkelly.com/',
-  },
-  {
-    title: 'CV Generator',
-    description:
-      'A React CV Generator. Users can add additional sections for experience, education, or their own custom sections. A print preview is updated live and can be saved as a PDF.',
-    image: 'src/assets/cv-generator.jpg',
-    tags: ['React', 'PostCSS', 'Vite'],
-    github: 'https://github.com/columk1/cv-application',
-    url: 'https://odin-cv-generator.netlify.app/',
-  },
-  {
-    title: 'Members Only',
-    description:
-      'An SSR message board app using Node, Express and MongoDB. Uses EJS for templating and PassportJS for authentication using session cookies.',
-    image: 'src/assets/members-only.jpg',
-    tags: ['Node', 'Express', 'MongoDB', 'EJS', 'PassportJS'],
-  },
-]
+import projects from '../../data/projects.js'
 
 const Projects = () => {
   return (
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,9 @@
-import cmsUrl from '../assets/blog-cms.jpg'
-import cvUrl from '../assets/cv-generator.jpg'
-import membersUrl from '../assets/members-only.jpg'
-
-export const projects = [
+const projects = [
   {
     title: 'Blog API',
     description:
       'A REST API for my personal blog using Node, Express and MongoDB. I built a CMS to interact with the API using React and Tailwind. It allows the admin to preview the markdown while editing. Authentication is handled using JWTs.',
-    image: cmsUrl,
+    image: '/images/blog-cms.jpg?url',
     tags: ['Node', 'Express', 'MongoDB', 'React', 'Tailwind'],
     github: 'https://github.com/columk1/blog-api',
     url: 'https://cms.columkelly.com/',
@@ -16,7 +12,7 @@ export const projects = [
     title: 'CV Generator',
     description:
       'A React CV Generator. Users can add additional sections for experience, education, or their own custom sections. A print preview is updated live and can be saved as a PDF.',
-    image: cvUrl,
+    image: '/images/cv-generator.jpg?url',
     tags: ['React', 'PostCSS', 'Vite'],
     github: 'https://github.com/columk1/cv-application',
     url: 'https://odin-cv-generator.netlify.app/',
@@ -25,7 +21,9 @@ export const projects = [
     title: 'Members Only',
     description:
       'An SSR message board app using Node, Express and MongoDB. Uses EJS for templating and PassportJS for authentication using session cookies.',
-    image: membersUrl,
+    image: '/images/members-only.jpg',
     tags: ['Node', 'Express', 'MongoDB', 'EJS', 'PassportJS'],
   },
 ]
+
+export default projects

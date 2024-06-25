0f3b667ddef5308b3b97d07ebb56f0aaf2d18e14
Colum Kelly <columk1@users.noreply.github.com>
2024-06-25 00:30:28 -0700

    Add projects, edit descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: 'Blog API',
     description:
-      'A REST API for my personal blog using Node, Express and MongoDB. I built a simple CMS to interact with the API using React and Tailwind. It allows the admin to preview the rendered markdown and syntax highlighting while editing. Authentication is handled using JWTs.',
+      'A REST API for my personal blog using Node, Express and MongoDB. I built a simple CMS to interact with the API using React and Tailwind. It includes a markdown previewer with syntax highlighting. Authentication is handled using JWTs via access and refresh tokens stored in HTTP-only cookies.',
     image: '/images/blog-cms.png',
     tags: ['Node', 'Express', 'MongoDB', 'React', 'Tailwind'],
     liveUrl: 'https://cms.columkelly.com/',
@@ -11,7 +11,7 @@ const projects = [
   {
     title: 'Messaging App',
     description:
-      'A real-time messaging app built with Next.js using React, Tailwind, Prisma, Postgres, NextAuth and Pusher. Although initially modelled on a tutorial with a slightly different stack, I spent a few weeks addressing issues and adding new features.',
+      'A real-time messaging app built with Next.js using React, Tailwind, Prisma, Postgres, NextAuth and Pusher. Although initially modelled on a tutorial with a slightly different stack, I spent a few weeks addressing issues, improving privacy, security and performance, replacing dependencies and adding new features of my own.',
     image: '/images/messaging-app.jpg',
     tags: ['NextJS', 'Typescript', 'Prisma', 'Postgres', 'Tailwind'],
     liveUrl: 'https://messaging-app-azure.vercel.app/',
@@ -49,7 +49,7 @@ const projects = [
   {
     title: 'Battleship',
     description:
-      'An MVC battleship game in Vanilla JS, built with Webpack. Drag and drop to position ships. Computer opponent uses a hunt and target algorithm.',
+      'An MVC battleship game in Vanilla JS, developed using TDD and built with Webpack. Drag and drop to position ships. Computer opponent uses a hunt and target algorithm.',
     image: '/images/battleship.png',
     tags: ['Javascript', 'HTML', 'CSS', 'Webpack'],
     liveUrl: 'https://top-battleship.netlify.app/',
@@ -63,6 +63,22 @@ const projects = [
     tags: ['Node', 'Express', 'MongoDB', 'EJS', 'PassportJS'],
     liveUrl: 'https://odin-members-only.adaptable.app',
     githubUrl: 'https://github.com/columk1/members-only',
+  },
+  {
+    title: 'Memory Game',
+    description: 'A memory game built in React. Players are scored based on speed and accuracy.',
+    image: '/images/memory-game.jpg',
+    tags: ['React', 'Vite'],
+    liveUrl: 'https://columk-memory-card.netlify.app/',
+    githubUrl: 'https://github.com/columk1/memory-card',
+  },
+  {
+    title: 'Pixel Pad',
+    description: 'A pixel art tool in vanilla JS. One of my first web projects.',
+    image: '/images/pixel-pad.png',
+    tags: ['Javascript', 'HTML', 'CSS'],
+    liveUrl: 'https://pixel-pad.netlify.app/',
+    githubUrl: 'https://github.com/columk1/pixel-pad',
   },
 ]
 

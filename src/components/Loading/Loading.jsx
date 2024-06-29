0a3d426ef01200a8cae17259fcf890b4ea73a795
Colum Kelly <columk1@users.noreply.github.com>
2024-06-28 23:59:20 -0700

    Add text beneath loading spinner

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,12 @@
 import styles from './Loading.module.css'
 import Spinner from '../Spinner/Spinner'
+import FadeInText from '../FadeInText/FadeInText'
+
 const Loading = () => {
   return (
     <div className={styles.loading}>
       <Spinner />
+      <FadeInText text='(Sorry, free tier...)' delay={500} />
     </div>
   )
 }
